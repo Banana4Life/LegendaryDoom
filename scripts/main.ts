f17a0b1c687567253f647c2695bc1cd40cb7d0bf
Phillip Schichtel <phillip@schich.tel>
2020-04-18 13:14:43 +0200

    browser accepts a wad file now

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,1 +1,39 @@
-alert('Hallo Typed World!')
+
+function dragStart(e: DragEvent): void {
+    if (e.target instanceof Element) {
+        e.target.classList.add('drag-hover')
+    }
+}
+function dragEnd(e: DragEvent): void {
+    if (e.target instanceof Element) {
+        e.target.classList.add('drag-hover')
+    }
+}
+
+document.querySelectorAll('.drop-target').forEach(elem => {
+    elem.addEventListener('dragenter', (e: DragEvent) => {
+        e.preventDefault()
+        dragStart(e)
+    })
+    elem.addEventListener('dragleave', (e: DragEvent) => {
+        dragEnd(e)
+    })
+
+    elem.addEventListener('dragover', e => {
+        e.preventDefault()
+    })
+
+    elem.addEventListener('drop', (e: DragEvent) => {
+        e.preventDefault()
+        dragEnd(e)
+        if (e.dataTransfer.files.length > 0) {
+            let possibleWad = e.dataTransfer.files.item(0)
+            if (possibleWad.type == "application/x-doom-wad") {
+                possibleWad.arrayBuffer().then(buf => {
+                    let memory = new Uint8Array(buf)
+                    console.log(memory[0], buf.byteLength)
+                })
+            }
+        }
+    })
+})
